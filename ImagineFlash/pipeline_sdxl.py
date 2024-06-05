bf6eb34833abb000a1a6f4694b9809dbf26150fb
shauray8 <shauray9@gmail.com>
2024-06-05 23:22:36 +0530

    scheduler and stuff

--- ImagineFlash/pipeline_sdxl.py
+++ ImagineFlash/pipeline_sdxl.py
@@ -14,6 +14,10 @@
 
 import inspect
 from typing import Any, Callable, Dict, List, Optional, Tuple, Union
+from dataclasses import dataclass
+from typing import List, Union
+import numpy as np
+import PIL.Image
 
 import torch
 from transformers import (
@@ -42,7 +46,6 @@ from ...models.attention_processor import (
     XFormersAttnProcessor,
 )
 from ...models.lora import adjust_lora_scale_text_encoder
-from ...schedulers import KarrasDiffusionSchedulers
 from ...utils import (
     USE_PEFT_BACKEND,
     deprecate,
@@ -55,7 +58,39 @@ from ...utils import (
 )
 from ...utils.torch_utils import randn_tensor
 from ..pipeline_utils import DiffusionPipeline, StableDiffusionMixin
+from ...utils import BaseOutput, is_flax_available
 
+## move to noise corr scheduler
+from ...schedulers import KarrasDiffusionSchedulers
+
+@dataclass
+class StableDiffusionXLPipelineOutput(BaseOutput):
+    """
+    Output class for Stable Diffusion pipelines.
+
+    Args:
+        images (`List[PIL.Image.Image]` or `np.ndarray`)
+            List of denoised PIL images of length `batch_size` or numpy array of shape `(batch_size, height, width,
+            num_channels)`. PIL images or numpy array present the denoised images of the diffusion pipeline.
+    """
+
+    images: Union[List[PIL.Image.Image], np.ndarray]
+
+
+if is_flax_available():
+    import flax
+
+    @flax.struct.dataclass
+    class FlaxStableDiffusionXLPipelineOutput(BaseOutput):
+        """
+        Output class for Flax Stable Diffusion XL pipelines.
+
+        Args:
+            images (`np.ndarray`)
+                Array of shape `(batch_size, height, width, num_channels)` with images from the diffusion pipeline.
+        """
+
+        images: np.ndarray
 
 if is_invisible_watermark_available():
     from .watermark import StableDiffusionXLWatermarker
